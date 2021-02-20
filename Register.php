e1809250830576033d6594446d7c52c39e672a61
megliryan <megliryan@gmail.com>
2021-02-19 17:40:05 -0700

    Login updates

--- Register.php
+++ Register.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Login.css">
     <title>Register</title>
 </head>
 <body>
-    Please enter your email here: <input type="text" id="email" placeholder="Email"><br>
-    Please enter your desired Username: <input type="text" id="username" placeholder="Username"><br>
-    Please enter a password: <input type="password" id="password" placeholder="Password"><br>
+    <div class="reset-password-container">
+        Please enter your email here:<br>
+        Please enter your desired Username:<br>
+        Please enter a password:<br>
+    </div>
+    <div>
+        <input type="text" id="email" placeholder="Email"><br>
+        <input type="text" id="username" placeholder="Username"><br>
+        <input type="password" id="password" placeholder="Password">
+    </div>
     <button type="submit">Register</button>
 </body>
 </html>
