66cab15dd298aa6ccf05724d48be99ede7e6bd5d
megliryan <megliryan@gmail.com>
2021-02-24 17:21:05 -0700

    Layout changes

--- Players.php
+++ Players.php
@@ -1,1 +1,12 @@
-These are the players
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Players</title>
+</head>
+<body>
+    These are the players
+</body>
+</html>
--- SprayChart.php
+++ SprayChart.php
@@ -1,2 +1,13 @@
-This is the spray chart
-<button><a href="Register.php">Add New User</a></button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spray Chart</title>
+</head>
+<body>
+    This is the spray chart
+    <button><a href="Register.php">Add New User</a></button>
+</body>
+</html>
