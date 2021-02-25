4fc060d5f7d6993c0da0fbdc919a6cd8c57e0bc8
megliryan <megliryan@gmail.com>
2021-02-24 17:28:13 -0700

    new pages

--- Forms.php
+++ Forms.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Reset Password</title>
+    <title>Forms</title>
 </head>
 <body>
-    Good job forgetting your password
+    Here are the forms
 </body>
 </html>
--- Main.php
+++ Main.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>MMHS Baseball</title>
 </head>
 <body>
@@ -12,6 +13,8 @@
         <button> <a href="Login.php">Login</a></button>
         <button> <a href="Players.php">Players</a></button>
         <button> <a href="Schedule.php">Schedule</a></button>
-    </div>
+        <button> <a href="Forms.php">Forms</a></button>
+    </div><br><br>
+    <div class="front-page">This is the where we will put whatever we choose, video, who's on base, etc</div>
 </body>
 </html>
