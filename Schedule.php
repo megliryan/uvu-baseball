b546035bd0af70c2ab356e4c407035c379bbb220
Mark Young <41711761+markthayneyoung@users.noreply.github.com>
2021-02-25 20:52:41 -0700

    Added navbar to all pages.
    Added navbar to all pages. Also worked on main page. Some pages renamed to .html files for css purposes.

--- Schedule.php
+++ Schedule.php
@@ -11,6 +11,43 @@
     <title>Calendar</title>
 </head>
 <body>
-This is the schedule.
+
+<!-- NAVIGATION BAR. DO NOT TOUCH. -->
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <!--<a class="navbar-brand" href="#">Navbar</a>-->
+  <div id=MMLogo>
+    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
+  </div>
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" href="Main.html">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Schedule.php">Schedule</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Players.php">Roster</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Players.php">Forms</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+<!-- END OF NAVIGATION BAR. DO NOT TOUCH. -->
+
 </body>
 </html>
