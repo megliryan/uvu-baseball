a5375a9817a2b117f04e7caa0702e33195f1ee1c
SirTeddylll <SirTeddylll@users.noreply.github.com>
2021-02-25 18:39:58 -0700

    added styling and updated main.php

--- Main.php
+++ Main.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +11,55 @@
     <title>MMHS Baseball</title>
 </head>
 <body>
-    <h1>Welcome to MMHS Baseball</h1>
-    <div>
-        <button> <a href="Login.php">Login</a></button>
-        <button> <a href="Players.php">Players</a></button>
-        <button> <a href="Schedule.php">Schedule</a></button>
-        <button> <a href="Forms.php">Forms</a></button>
-    </div><br><br>
+
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <!--<a class="navbar-brand" href="#">Navbar</a>-->
+  <div id=MMLogo>
+    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
+  </div>
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Schedule</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Roster
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="jumbotron">
+    <h1>Maple Mountain High School Baseball</h1>      
+    <p>Welcome to the mountain!</p>
+</div>
+
+<div class="row">
+  <div class="col-sm-8">
+  <div class="jumbotron">
+    <h2>Top Stories</h2>
+    </div>
+  </div>
+
+<div class="col-sm-4">
+    <div class="jumbotron">
+      <h2>Livestream</h2>
+      </div>
+  </div>
+</div>
+
+
     <div class="front-page">This is the where we will put whatever we choose, video, who's on base, etc</div>
 </body>
 </html>
