de8652179145540c1250f21cf7dc17318dc3ddc1
nathangmahler1987 <nathangmahler@gmail.com>
2021-03-01 21:02:44 -0700

    Schedule Correct

--- Schedule.php
+++ Schedule.php
@@ -53,23 +53,17 @@
 </nav>
 <!-- END OF NAVIGATION BAR. DO NOT TOUCH. -->
 
-<div class="row no-gutters align-items-center">
-    <div class= "col no-gutters">
-      
-      <div class="container-fluid text-center fill-height-schedule"><div class="row" >dfghkjhkjhkjh</div></div>
-      
-      <div class="row">
-        <div class="container-fluid fill-height-schedule text-center">dfghkjhkjhkjh</div>
-      </div>
-      <div class="row">
-        <div class="container-fluid fill-height-schedule text-center">dfghkjhkjhkjh</div>
-      </div>
+<div class="container-fluid" style="height: 100vh">
+  <div class="row h-100 no-gutters">
+    <div class = "col-sm-6 d-flex flex-column">
+          <div class="bg-info align-items-center flex-grow-1 justify-content-center d-flex"><div>left1</div></div>  
+          <div class="bg-danger align-items-center flex-grow-1 justify-content-center d-flex"><div>left2</div></div>
+          <div class="bg-success align-items-center flex-grow-1 justify-content-center d-flex"><div>left3</div></div>  
     </div>
-    <div class = "col no-gutters">
-        <div class="d-flex justify-content-center align-items-center">
-            <img " src="images/MMHS_Logo2.jpg" alt="School Logo" >
-        </div>
+    <div class = "col-sm-6 bg-warning">
+       <img class="img-fluid" src="images/MMHS_Logo2.jpg" alt="School Logo" >
     </div>
+  </div>
 </div>
 
 </body>
