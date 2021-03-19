91c816d1259e85c68010231fd8bdd9c7c27f73ad
wowbchadtyler <60561525+wowbchadtyler@users.noreply.github.com>
2021-03-19 17:07:08 -0600

    button invisi
    added a button that is only visible if admin is logged in

--- Players.php
+++ Players.php
@@ -54,5 +54,54 @@
 </nav>
 <!-- END OF NAVIGATION BAR. DO NOT TOUCH. -->
 
+
+
+
+
+
+
+
+
+
+
+<!-- section bellow is for uploading and displaying players info-->
+
+
+<!-- display a button only if user is logged in -->
+
+<?php
+session_start();
+if($_SESSION['isAdmin']) : ?><!--will need to link to admin page to check if logged in (need ryan to use "isadmin"
+                                  variable on his page for login)-->
+  <span>Edit</span>
+<?php endif; ?>
+<form method="POST">
+  <div style="position: absolute; right:50px; bottom:100px">
+    <button id="upload" type="submit">Upload</button>
+  </div>  
+  
+  <!--if button is clicked it will then upload a new statsbar with picture that was uploaded.
+  will need to figure out how to pull multiple fields for stats on each player (do i link the stats to player name?)-->
+  <?php
+  if(isset($_POST))
+  {
+    
+  }
+  ?>
+</form>
+
+<!-- statsbar-->
+<div>
+  <img class="center image" src="images/StatsBar.png" alt="centered image" width="800px" >
+</div>
+
 </body>
 </html>
+
+
+
+  
+
+  
+
+
