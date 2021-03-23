8cc56b4f7ac26eca694745bc680a20630787827c
megliryan <megliryan@gmail.com>
2021-03-23 13:26:37 -0600

    nav bar

--- welcome.php
+++ welcome.php
@@ -3,10 +3,10 @@
 session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//     header("location: login.php");
+//     exit;
+// }
 ?>
  
 <!DOCTYPE html>
@@ -20,6 +20,44 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <!--<a class="navbar-brand" href="#">Navbar</a>-->
+  <div id=MMLogo>
+    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
+  </div>
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Schedule.php">Schedule</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Players.php">Roster</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Forms.html">Forms</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Logout.php">Logout</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
