5ce0d42f740a334124fc9bc29a977212f39fce57
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-03-26 17:34:18 -0600

    Revert "Attach 'views' navbar to main pages"
    This reverts commit 2c794ff57b7f0162c01c340a3f1401b8c52ca539.

--- Schedule.php
+++ Schedule.php
@@ -1,4 +1,58 @@
-<?php include('views/header.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- bootstrap declaration -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Calendar</title>
+</head>
+<body>
+
+<!-- NAVIGATION BAR. DO NOT TOUCH. -->
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand -->
+  <!--<a class="navbar-brand" href="#">Navbar</a>-->
+  <div id=MMLogo>
+    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
+  </div>
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Schedule.php">Schedule</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Players.php">Roster</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Forms.html">Forms</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Login.php">Login</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+<!-- END OF NAVIGATION BAR. DO NOT TOUCH. -->
 
 <div class="container-fluid" style="height: 100vh">
   <div class="row h-100 no-gutters">
@@ -12,5 +66,5 @@
     </div>
   </div>
 </div>
-
-<?php include('views/footer.php');?>
+</body>
+</html>
