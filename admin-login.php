c1a75026f3807aba7c68a9291b5c414ddbcad713
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-04-03 13:37:27 -0600

    adminusers > admins

--- admin-login.php
+++ admin-login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM admins WHERE username = :username";
+        $sql = "SELECT id, username, password FROM adminusers WHERE username = :username";
         
         if($stmt = $db->prepare($sql)){
             // Bind variables to the prepared statement as parameters
