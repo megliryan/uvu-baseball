c9feaf01d0fdab2bbb3333128389a019d501718c
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-04-06 21:53:26 -0600

    Solve some merge conflicts
    Yeah, the navbar was messed up.

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:index.php
-<?php
-=======
 <?php
 $title = "MMHS Baseball";
->>>>>>> 17a4ed5a8208c8fba58ba64a316aa4ed69e714f3:index.php
 /*php reference SQL database for current announcements data
 
 $servername = "localhost";
@@ -27,74 +23,10 @@ echo "Connected successfully";
 
 */
 
-<<<<<<< HEAD:index.html
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-<!-- bootstrap declaration -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-<!-- news carousel code-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>MMHS Baseball</title>
-</head>
-<body>
-  <!--declaration of navigation bar-->
-  <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-  <!-- Brand -->
-  <!--<a class="navbar-brand" href="#">Navbar</a>-->
-  <div id=MMLogo>
-    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
-  </div>
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Schedule.php">Schedule</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Players.php">Roster</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="forms/index.php">Forms</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Login.php">Login</a>
-      </li>
-
-    </ul>
-  </div>
-</nav>
-<br>
-=======
 
 include('views/header.php');
 ?>
 
->>>>>>> 17a4ed5a8208c8fba58ba64a316aa4ed69e714f3:index.php
 <!--declaration of div Title-->
 <div class="jumbotron">
     <h1>Maple Mountain Baseball</h1>      
