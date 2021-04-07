17a4ed5a8208c8fba58ba64a316aa4ed69e714f3
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-04-06 19:34:29 -0600

    Connect index, Players, and Schedule to views header/footer
    Not sure how the schedule page is set up, though. I'll work on it eventually.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$title = "MMHS Baseball";
 /*php reference SQL database for current announcements data
 
 $servername = "localhost";
@@ -22,61 +23,9 @@ echo "Connected successfully";
 
 */
 
+
+include('views/header.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-<!-- bootstrap declaration -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>MMHS Baseball</title>
-</head>
-<body>
-  <!--declaration of navigation bar-->
-  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <!-- Brand -->
-  <!--<a class="navbar-brand" href="#">Navbar</a>-->
-  <div id=MMLogo>
-    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
-  </div>
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Schedule.php">Schedule</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Players.php">Roster</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="forms/index.php">Forms</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Login.php">Login</a>
-      </li>
-
-    </ul>
-  </div>
-</nav>
 
 <!--declaration of div Title-->
 <div class="jumbotron jumbotron-fluid">
@@ -123,5 +72,4 @@ echo "Connected successfully";
   </div>
 </div>
 
-</body>
-</html>
+<?php include('views/footer.php');?>
