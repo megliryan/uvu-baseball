34962789868dd018c18f4e4b659d109699e6667c
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-04-07 09:51:28 -0600

    Add JS scripts to all pages

--- views/header.php
+++ views/header.php
@@ -21,6 +21,10 @@ $root = '/uvu-baseball/';
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?=$root?>styles.css">
 <title><?php if (isset($title)) {echo $title;} else {echo 'MMHS Baseball';}?></title>
+<!-- JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <?php if (isset($head)) {echo $head;}?>
 </head>
 <body>
