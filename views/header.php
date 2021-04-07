b4f1bb5fb1c22287fd0beba4951c8292fcb336ee
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-04-06 22:12:15 -0600

    Fix the header - now it can point anywhere
    I cause problems all the time

--- views/header.php
+++ views/header.php
@@ -1,7 +1,9 @@
 <?php 
 if (session_status() === PHP_SESSION_NONE) {
   session_start();
-}?>
+}
+$root = '/uvu-baseball/';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +15,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/styles.css">
+<link rel="stylesheet" href="<?=$root?>styles.css">
 <title>MMHS Baseball</title>
 </head>
 <body>
@@ -22,7 +24,7 @@ if (session_status() === PHP_SESSION_NONE) {
   <!-- Brand -->
   <!--<a class="navbar-brand" href="#">Navbar</a>-->
   <div id=MMLogo>
-    <a class="navbar-brand" href="#"><img src="/images/School_Logo.png" alt="Logo" style="width:60px;"></a>
+    <a class="navbar-brand" href="#"><img src="<?=$root?>images/School_Logo.png" alt="Logo" style="width:60px;"></a>
   </div>
   <!-- Toggler/collapsibe Button -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -34,35 +36,35 @@ if (session_status() === PHP_SESSION_NONE) {
     <ul class="navbar-nav">
 
       <li class="nav-item">
-        <a class="nav-link" href="/index.php">Home</a>
+        <a class="nav-link" href="<?=$root?>index.php">Home</a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="/Schedule.php">Schedule</a>
+        <a class="nav-link" href="<?=$root?>Schedule.php">Schedule</a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="/Players.php">Roster</a>
+        <a class="nav-link" href="<?=$root?>Players.php">Roster</a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="/forms">Forms</a>
+        <a class="nav-link" href="<?=$root?>forms">Forms</a>
       </li>
       <?php if (!isset($_SESSION['username'])):
         # If the user is logged out, show the login button.?>
       <li class="nav-item">
-        <a class="nav-link" href="/Login.php">Login</a>
+        <a class="nav-link" href="<?=$root?>Login.php">Login</a>
       </li>
       <?php endif?>
       <?php if (isset($_SESSION['is_admin'])):
         # If the user is logged in, show the logout button.?>
       <li class="nav-item">
-        <a class="nav-link" href="/Logout.php">Logout</a>
+        <a class="nav-link" href="<?=$root?>Logout.php">Logout</a>
       </li>
       <?php if ($_SESSION['is_admin']):
         # If previous & the user is an admin, show the manage button.?>
       <li class="nav-item">
-       <a class="nav-link" href="/admin">Manage</a>
+       <a class="nav-link" href="<?=$root?>admin.php">Manage</a>
       </li>
       <?php endif; endif; # Close navbar options.?>
 
