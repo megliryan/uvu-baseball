b5f6f5a2d9389593a6e70f5ce5aaf944e418419f
richardfoy <78106733+richardfoy@users.noreply.github.com>
2021-04-07 18:25:35 -0600

    Add new navbar to adminregister page.

--- adminregister.php
+++ adminregister.php
@@ -102,63 +102,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     //mysqli_close($link);
 }
+$title = "Admin Register";
+include('views/header.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-<!-- bootstrap declaration -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Register</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <!-- Brand -->
-  <!--<a class="navbar-brand" href="#">Navbar</a>-->
-  <div id=MMLogo>
-    <a class="navbar-brand" href="#"><img src="images/School_Logo.png" alt="Logo" style="width:60px;"></a>
-  </div>
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Schedule.php">Schedule</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Players.php">Roster</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="forms/index.php">Forms</a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="Login.php">Login</a>
-      </li>
-
-    </ul>
-  </div>
-</nav>
 <div class="wrapper">
         <h2>Sign Up</h2>
-        <p class="white-text">Please fill this form to create an account.</p>
+        <p class="white-text">Please fill out this form to create an <strong>administrator</strong> account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
                 <label>Username</label>
@@ -180,5 +129,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
         </form>
     </div>
-</body>
-</html>
+<?php include('views/footer.php');?>
